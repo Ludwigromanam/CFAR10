90dd36a2b0793279d498f7f24895d650cdd04618
Pierce Spitler <pierce.spitler@eyeqinsights.com>
2016-02-19 14:19:01 -0600

    Removed run training and adjusted base script

--- base_script.sh
+++ base_script.sh
@@ -1,3 +1,2 @@
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 python data_create.py
-python run_training.py
--- run_training.py
+++
@@ -1,4 +0,0 @@
-from basic_convnet_model import main
-
-if __name__ == '__main__':
-    main()
