5374bb200c8df4c42907b1b2ea856573dc8fa2bc
Pierce Spitler <pierce.spitler@eyeqinsights.com>
2016-02-20 22:33:15 -0600

    Made changes to be AWS compliant since I run in the cloud now.

--- read_data.py
+++ read_data.py
@@ -56,10 +56,10 @@ def read_data(filename_queue):
     _, serialized_example = reader.read(filename_queue)
     features = tf.parse_single_example(
         serialized_example,
-        dense_keys=['image_raw', 'label'],
-        dense_types=[tf.string, tf.int64]
-        #features={'image_raw': tf.FixedLenFeature([], tf.string),
-        #          'label': tf.FixedLenFeature([], tf.int64)}
+        #dense_keys=['image_raw', 'label'],
+        #dense_types=[tf.string, tf.int64]
+        features={'image_raw': tf.FixedLenFeature([], tf.string),
+                  'label': tf.FixedLenFeature([], tf.int64)}
     )
     image = tf.decode_raw(features['image_raw'], tf.uint8)
     image.set_shape([FLAGS.input_image_size * FLAGS.input_image_size * FLAGS.input_image_channels])
