44def8cfb8bb0b29f32dd16e73791f06e2a46554
Pierce Spitler <pierce.spitler@eyeqinsights.com>
2016-02-25 16:12:25 -0600

    PEP8

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -21,99 +21,98 @@ fcdepth = 1024
 
 
 def inference(images):
-  # Variables.
-  l1conv1_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, convdepth1],
-                                                   stddev=0.03))
-  l1conv1_bias = tf.Variable(tf.zeros([convdepth1]))
-  l1conv2_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth1, convdepth1],
-                                                   stddev=0.03))
-  l1conv2_bias = tf.Variable(tf.constant(0.01, shape=[convdepth1]))
+    # Variables.
+    l1conv1_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, convdepth1],
+                                                     stddev=0.03))
+    l1conv1_bias = tf.Variable(tf.zeros([convdepth1]))
+    l1conv2_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth1, convdepth1],
+                                                     stddev=0.03))
+    l1conv2_bias = tf.Variable(tf.constant(0.01, shape=[convdepth1]))
 
-  l2conv1_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth1, convdepth2],
-                                                   stddev=0.03))
-  l2conv1_bias = tf.Variable(tf.constant(0.01, shape=[convdepth2]))
-  l2conv2_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth2, convdepth2],
-                                                   stddev=0.03))
-  l2conv2_bias = tf.Variable(tf.constant(0.01, shape=[convdepth2]))
+    l2conv1_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth1, convdepth2],
+                                                     stddev=0.03))
+    l2conv1_bias = tf.Variable(tf.constant(0.01, shape=[convdepth2]))
+    l2conv2_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth2, convdepth2],
+                                                     stddev=0.03))
+    l2conv2_bias = tf.Variable(tf.constant(0.01, shape=[convdepth2]))
 
-  l3conv1_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth2, convdepth3],
-                                                   stddev=0.03))
-  l3conv1_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
-  l3conv2_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth3, convdepth3],
-                                                   stddev=0.03))
-  l3conv2_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
-  l3conv3_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth3, convdepth3],
-                                                   stddev=0.03))
-  l3conv3_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
-  l3conv4_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth3, convdepth3],
-                                                   stddev=0.03))
-  l3conv4_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
+    l3conv1_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth2, convdepth3],
+                                                     stddev=0.03))
+    l3conv1_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
+    l3conv2_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth3, convdepth3],
+                                                     stddev=0.03))
+    l3conv2_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
+    l3conv3_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth3, convdepth3],
+                                                     stddev=0.03))
+    l3conv3_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
+    l3conv4_weight = tf.Variable(tf.truncated_normal([patch_size, patch_size, convdepth3, convdepth3],
+                                                     stddev=0.03))
+    l3conv4_bias = tf.Variable(tf.constant(0.01, shape=[convdepth3]))
 
-  fc1_weight = tf.Variable(tf.truncated_normal([image_size/(2*2*2), image_size/(2*2*2), convdepth3, fcdepth],
-                                               stddev=0.05))
-  fc1_bias = tf.Variable(tf.constant(0.01, shape=[1, 1, fcdepth]))
-  fc2_weight = tf.Variable(tf.truncated_normal([1, 1, fcdepth, fcdepth], stddev=0.05))
-  fc2_bias = tf.Variable(tf.constant(0.01, shape=[1, 1, fcdepth]))
-  out_weight = tf.Variable(tf.truncated_normal([1, 1, fcdepth, num_labels], stddev=0.05))
-  out_bias = tf.Variable(tf.constant(0.0, shape=[1, 1, num_labels]))
+    fc1_weight = tf.Variable(
+        tf.truncated_normal([image_size / (2 * 2 * 2), image_size / (2 * 2 * 2), convdepth3, fcdepth],
+                            stddev=0.05))
+    fc1_bias = tf.Variable(tf.constant(0.01, shape=[1, 1, fcdepth]))
+    fc2_weight = tf.Variable(tf.truncated_normal([1, 1, fcdepth, fcdepth], stddev=0.05))
+    fc2_bias = tf.Variable(tf.constant(0.01, shape=[1, 1, fcdepth]))
+    out_weight = tf.Variable(tf.truncated_normal([1, 1, fcdepth, num_labels], stddev=0.05))
+    out_bias = tf.Variable(tf.constant(0.0, shape=[1, 1, num_labels]))
 
-  l1conv1 = tf.nn.conv2d(images, l1conv1_weight, [1, 1, 1, 1], padding='SAME')
-  l1relu1 = tf.nn.relu(l1conv1 + l1conv1_bias)
-  l1conv2 = tf.nn.conv2d(l1relu1, l1conv2_weight, [1, 1, 1, 1], padding='SAME')
-  l1relu2 = tf.nn.relu(l1conv2 + l1conv2_bias)
-  l1pool = tf.nn.max_pool(l1relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    l1conv1 = tf.nn.conv2d(images, l1conv1_weight, [1, 1, 1, 1], padding='SAME')
+    l1relu1 = tf.nn.relu(l1conv1 + l1conv1_bias)
+    l1conv2 = tf.nn.conv2d(l1relu1, l1conv2_weight, [1, 1, 1, 1], padding='SAME')
+    l1relu2 = tf.nn.relu(l1conv2 + l1conv2_bias)
+    l1pool = tf.nn.max_pool(l1relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-  l2conv1 = tf.nn.conv2d(l1pool, l2conv1_weight, [1, 1, 1, 1], padding='SAME')
-  l2relu1 = tf.nn.relu(l2conv1 + l2conv1_bias)
-  l2conv2 = tf.nn.conv2d(l2relu1, l2conv2_weight, [1, 1, 1, 1], padding='SAME')
-  l2relu2 = tf.nn.relu(l2conv2 + l2conv2_bias)
-  l2pool = tf.nn.max_pool(l2relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    l2conv1 = tf.nn.conv2d(l1pool, l2conv1_weight, [1, 1, 1, 1], padding='SAME')
+    l2relu1 = tf.nn.relu(l2conv1 + l2conv1_bias)
+    l2conv2 = tf.nn.conv2d(l2relu1, l2conv2_weight, [1, 1, 1, 1], padding='SAME')
+    l2relu2 = tf.nn.relu(l2conv2 + l2conv2_bias)
+    l2pool = tf.nn.max_pool(l2relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-  l3conv1 = tf.nn.conv2d(l2pool, l3conv1_weight, [1, 1, 1, 1], padding='SAME')
-  l3relu1 = tf.nn.relu(l3conv1 + l3conv1_bias)
-  l3conv2 = tf.nn.conv2d(l3relu1, l3conv2_weight, [1, 1, 1, 1], padding='SAME')
-  l3relu2 = tf.nn.relu(l3conv2 + l3conv2_bias)
-  l3conv3 = tf.nn.conv2d(l3relu2, l3conv3_weight, [1, 1, 1, 1], padding='SAME')
-  l3relu3 = tf.nn.relu(l3conv3 + l3conv3_bias)
-  l3conv4 = tf.nn.conv2d(l3relu3, l3conv4_weight, [1, 1, 1, 1], padding='SAME')
-  l3relu4 = tf.nn.relu(l3conv4 + l3conv4_bias)
-  l3pool = tf.nn.max_pool(l3relu4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    l3conv1 = tf.nn.conv2d(l2pool, l3conv1_weight, [1, 1, 1, 1], padding='SAME')
+    l3relu1 = tf.nn.relu(l3conv1 + l3conv1_bias)
+    l3conv2 = tf.nn.conv2d(l3relu1, l3conv2_weight, [1, 1, 1, 1], padding='SAME')
+    l3relu2 = tf.nn.relu(l3conv2 + l3conv2_bias)
+    l3conv3 = tf.nn.conv2d(l3relu2, l3conv3_weight, [1, 1, 1, 1], padding='SAME')
+    l3relu3 = tf.nn.relu(l3conv3 + l3conv3_bias)
+    l3conv4 = tf.nn.conv2d(l3relu3, l3conv4_weight, [1, 1, 1, 1], padding='SAME')
+    l3relu4 = tf.nn.relu(l3conv4 + l3conv4_bias)
+    l3pool = tf.nn.max_pool(l3relu4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-  fcconv1 = tf.nn.conv2d(l3pool, fc1_weight, [1, 1, 1, 1], padding='SAME')
-  fcrelu1 = tf.nn.relu(fcconv1 + fc1_bias)
-  fcconv2 = tf.nn.conv2d(fcrelu1, fc2_weight, [1, 1, 1, 1], padding='SAME')
-  fcrelu2 = tf.nn.relu(fcconv2 + fc2_bias)
+    fcconv1 = tf.nn.conv2d(l3pool, fc1_weight, [1, 1, 1, 1], padding='SAME')
+    fcrelu1 = tf.nn.relu(fcconv1 + fc1_bias)
+    fcconv2 = tf.nn.conv2d(fcrelu1, fc2_weight, [1, 1, 1, 1], padding='SAME')
+    fcrelu2 = tf.nn.relu(fcconv2 + fc2_bias)
 
-  output = tf.nn.conv2d(fcrelu2, out_weight, [1, 1, 1, 1], padding='SAME') + out_bias
-  scaled_up_preds = tf.image.resize_images(output, new_image_size, new_image_size)
-  logits = tf.reshape(scaled_up_preds, [-1, num_labels])
+    output = tf.nn.conv2d(fcrelu2, out_weight, [1, 1, 1, 1], padding='SAME') + out_bias
+    scaled_up_preds = tf.image.resize_images(output, new_image_size, new_image_size)
+    logits = tf.reshape(scaled_up_preds, [-1, num_labels])
 
-  return logits
+    return logits
 
 
 def run_model_image(image):
     with tf.Graph().as_default():
+        image = tf.reshape(image, [new_image_size, new_image_size, 3])
+        image = tf.image.per_image_whitening(image)
+        image = tf.reshape(image, [1, new_image_size, new_image_size, 3])
+        image = tf.cast(image, tf.float32)
 
-      image = tf.reshape(image, [new_image_size, new_image_size, 3])
-      image = tf.image.per_image_whitening(image)
-      image = tf.reshape(image, [1, new_image_size, new_image_size, 3])
-      image = tf.cast(image, tf.float32)
+        train_prediction = inference(images=image)
 
-      train_prediction = inference(images=image)
+        saver = tf.train.Saver(tf.all_variables())
+        sess = tf.Session()
+        saver.restore(sess=sess, save_path='vd_tmodel.ckpt')
+        predictions = sess.run(train_prediction)
 
-      saver = tf.train.Saver(tf.all_variables())
-      sess = tf.Session()
-      saver.restore(sess=sess, save_path='vd_tmodel.ckpt')
-      predictions = sess.run(train_prediction)
-
-
-      return predictions
+        return predictions
 
 
-image = cv2.imread('/Users/pspitler3/Documents/caffe_images/dogandcat.jpg')
+read_image = cv2.imread('/Users/pspitler3/Documents/caffe_images/dogandcat.jpg')
 # image = image[40:320, :, :]
-image = cv2.resize(image, (new_image_size, new_image_size), interpolation=cv2.INTER_AREA)
-imdata = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+read_image = cv2.resize(read_image, (new_image_size, new_image_size), interpolation=cv2.INTER_AREA)
+imdata = cv2.cvtColor(read_image, cv2.COLOR_BGR2RGB)
 
 t = datetime.fromtimestamp(time(), None)
 
